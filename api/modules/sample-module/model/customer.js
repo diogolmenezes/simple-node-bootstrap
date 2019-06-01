1a83c0e3ceb92dabd31d241fcbce137fdca079f0
Diogo Menezes <diogolmenezes@gmail.com>
2019-06-01 15:43:57 -0300

    updating to 2.0

--- api/modules/sample-module/model/customer.js
+++ api/modules/sample-module/model/customer.js
@@ -1,8 +1,8 @@
 const { database } = require('simple-node-framework').Singleton;
 const mongoose = require('mongoose');
 
-// on database.connections.application, application is the conection name. to use another connection change this name
-const connection = database.connections.application || mongoose;
+// on database.connections.mongodb.application, application is the conection name. to use another connection change this name
+const connection = database.connections.mongodb ? database.connections.mongodb.application : mongoose || mongoose;
 
 const schema = mongoose.Schema({
     nome: String,
